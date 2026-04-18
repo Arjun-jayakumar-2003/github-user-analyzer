61d7561ccadb8c1f2b6972792ad327affbc24c33
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-04-18 18:52:13 +0530

    Integrate GitHub API to fetch and return user data

--- github_insights/views.py
+++ github_insights/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
+import os
 
 @api_view(['GET'])
 def test_api(request, username):
-    return Response({"message" : f"Api is working! Hello {username}"})
+
+    token = os.environ.get("GITHUB_TOKEN")
+    headers = {
+        "Authorization" : f"token {token}"
+    }
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    return Response(data, status=response.status_code)
