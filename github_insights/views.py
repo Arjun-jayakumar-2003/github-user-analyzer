60c94d8ad7f3539cfc2c9edae972a106344cb61f
Arjun Jayakumar <arjunjayakumar34@gmail.com>
2026-04-19 06:48:32 +0530

    Add data processing to extract repo count, total stars, and top language from GitHub API

--- github_insights/views.py
+++ github_insights/views.py
@@ -25,9 +25,30 @@ def test_api(request, username):
     except Exception:
         return Response({"error" : "Something went wrong"}, status=503)
     
-
-    
-    
     data = response.json()
+    
+    repos_url = f"https://api.github.com/users/{username}/repos"
+    repos_response = requests.get(repos_url, headers=headers)
+    repos_data = repos_response.json()
 
-    return Response(data, status=response.status_code)
+    total_stars = sum(repo.get("stargazers_count" , 0) for repo in repos_data)
+
+    language_count = {}
+
+    for repo in repos_data:
+        lang = repo.get("language")
+        if lang:
+            language_count[lang] = language_count.get(lang , 0) + 1
+    
+    top_language = max(language_count , key=language_count.get) if language_count else None
+    
+    repo_count = data.get("public_repos" , 0)
+
+
+    processed_data = {
+        "Username" : data.get("login"),
+        "repo_count" : repo_count,
+        "total_stars" : total_stars,
+        "top_language" : top_language
+    }
+    return Response(processed_data , status=response.status_code)
